ba8dd911bea9a1799dd32e3b1fb1b4132874f024
hawaiihangover <hawaiihangover@gmail.com>
2015-05-05 17:45:05 -0500

    Update hhis.py

--- hhis.py
+++ hhis.py
@@ -1,4 +1,12 @@
 from hhproduct import *
+from rakuten import *
 
 palmwave = Supplier()
-palmwave.parse_excel('palmwave.xlsm')
+palmwave.parse_excel('Palmwave.xlsm')
+
+mf = RakutenFace()
+
+for c in palmwave:
+    mf.rakuten_new_product_file = c.name + '.csv'
+    dl = mf.create_data(c)
+    mf.write_data(dl)
