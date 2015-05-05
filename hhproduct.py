ff7e2edb470a9acd7dc389cc6864e2413a85fd0d
hawaiihangover <hawaiihangover@gmail.com>
2015-05-05 17:44:33 -0500

    Update hhproduct.py

--- hhproduct.py
+++ hhproduct.py
@@ -8,6 +8,7 @@ from os.path import basename, splitext
 import openpyxl as exl
 import re
 
+
 class Supplier(object):
     
     def __init__(self):
@@ -19,6 +20,9 @@ class Supplier(object):
         
     def __str__(self):
         return self.name
+        
+    def __iter__(self):
+        return iter(self.collections)
         
     def product_count(self):
         count = 0
@@ -63,8 +67,6 @@ class Supplier(object):
             self.collections.append(collection)
             
         self._add_patterns()
-                    
-            
 
 
 class Collection(object):
@@ -72,22 +74,32 @@ class Collection(object):
     def __init__(self):
     
         self.name = ''
-        self.description = ''
-        self.sizes = []
+        self.title = ''
         self.patterns = set()
-        self.cost_map = {}
-        self.price_map = {}
         self.supplier = None
         self.products = []
+        self.tags = []
         
         self.namere = ''
+        
+        self.magento_description = ''
         self.magento_attribute_set= ''
         self.magento_category = []
-        self.magento_attribute_code = 'size_sto4xl'
+        self.magento_attribute_code = ''
+        
+        self.rakuten_description = ''
+        self.rakuten_features = ''
+        self.rakuten_age_gender = ''
+        self.rakuten_material = ''
+        self.rakuten_cat_id = ''
+        self.rakuten_top_style = ''
         
         
     def __str__(self):
         return self.name
+        
+    def __iter__(self):
+        return iter(self.products)
         
     def _add_patterns(self):
         for p in self.products:
@@ -100,57 +112,47 @@ class Collection(object):
                 match_list.append(p)
                 
         return match_list
-                
+
+
     def product_count(self):
         return len(self.products)
         
-    def _parse_size_segment(self, sheet, row_n, id):
-        
-        if sheet['A'+str(row_n)].value != id:
-            return None
-            
-        smap = {}
-        if len(self.sizes) == 1:
-            smap['onesize'] = sheet['B'+str(row_n)].value
-            row_n += 1
-        else:
-            while (sheet['A'+str(row_n)].value == "" or
-                   sheet['A'+str(row_n)].value is None or
-                   sheet['A'+str(row_n)].value == id):
-                if (sheet['B'+str(row_n)].value is None or
-                    sheet['B'+str(row_n)].value == ""):
-                   break
-                szs = sheet['B'+str(row_n)].value.split(',')
-                price = sheet['C'+str(row_n)].value
-                for s in szs:
-                    smap[s] = price
-                row_n += 1
-            
-        return smap, row_n
+
+    def locate_row(self, sheet, text):
+        i = 1
+        while sheet['A'+str(i)].value != text:
+            i += 1
+        return i
 
                 
     def parse_sheet(self, sheet):
         
-        self.sizes = sheet['B1'].value.split(',')
-        self.name = sheet['B2'].value
+        self.name = sheet.title
+        self.title = sheet['B2'].value
         self.namere = sheet['B3'].value
-        self.description = sheet['B4'].value
-        self.magento_attribute_set= sheet['B5'].value
-        self.magento_category = sheet['B6'].value.split(',')
         
-        row_n = 7
-        self.price_map, row_n = self._parse_size_segment(sheet, row_n, 'Price')
-        self.cost_map, row_n = self._parse_size_segment(sheet, row_n, 'Cost')
-        row_n += 2
+        self.magento_description = sheet['F4'].value
+        self.magento_attribute_set= sheet['F2'].value
+        self.magento_category = sheet['F3'].value.split(',')
+        
+        self.rakuten_description = sheet['I2'].value
+        self.rakuten_features = sheet['I3'].value
+        self.rakuten_age_gender = sheet['I4'].value
+        self.rakuten_material = sheet['I5'].value
+        self.rakuten_cat_id = sheet['I6'].value
+        self.rakuten_top_style = sheet['I7'].value
 
-        while sheet['A'+str(row_n)].value is not None:
-            if (sheet['B'+str(row_n)].value is not None and
-                sheet['B'+str(row_n)].value != ""):
-                product = Product();
+        sku_row = self.locate_row(sheet, 'SKU') + 1
+               
+        while sheet['A'+str(sku_row)].value is not None:
+            if (sheet['C'+str(sku_row)].value is not None and
+                sheet['C'+str(sku_row)].value != ""):
+                product = Product()
                 product.collection = self
-                product.parse_row(sheet, row_n)
+                sku_row = product.parse_row(sheet, sku_row)
                 self.products.append(product)
-            row_n += 1
+            else:
+                sku_row += 1
             
         self._add_patterns()
     
@@ -164,13 +166,18 @@ class Product(object):
         self.name = ''
         self.pattern = ''
         self.collection = None
-        
         self.color = []
+        self.tags = []
+        self.variations = []
+        self.image = ''
+        self.alt_images = ''
         
     def __str__(self):
         return self.sku
         
-            
+    def __iter__(self):
+        return iter(self.variations)
+    
     def find_product(self, sku=None, exp=None):
         if sku is not None:
             return self.sku == sku
@@ -181,16 +188,61 @@ class Product(object):
                 return True
 
         return False
-        
 
     def parse_row(self, sheet, row_n):
         
         self.sku = sheet['A'+str(row_n)].value
-        self.pattern = sheet['B'+str(row_n)].value
+        self.pattern = sheet['C'+str(row_n)].value
+        self.image = sheet['J'+str(row_n)].value
+        self.alt_images = sheet['K'+str(row_n)].value
         
         if self.collection is not None:
-            self.name = re.sub('<Pattern>', self.pattern,
-                re.sub('<Name>', self.collection.name,
-                self.collection.namere))
+            namere = self.collection.namere
+            
+            if '<Name>' in namere:
+                namere = re.sub('<Name>', self.collection.name, namere)
+            if '<Pattern>' in namere:
+                namere = re.sub('<Pattern>', self.pattern, namere)
+            if '<Material>' in namere:
+                namere = re.sub('<Material>', self.collection.rakuten_material, namere)
+                
+            self.name = namere
         
+        row_n += 1
+        while sheet['B'+str(row_n)].value is not None:
+            variation = Variation()
+            variation.product = self
+            variation.parse_var(sheet, row_n)
+            self.variations.append(variation)
+            row_n += 1
+            
+        return row_n
 
+
+class Variation(object):
+    
+    def __init__(self):
+        
+        self.sku = ''
+        self.price = ''
+        self.cost = ''
+        self.weight = ''
+        self.upc = ''
+        self.size = ''
+        self.mfg_num = ''
+        self.product = None
+        
+    
+    def __str__(self):
+        print self.sku
+        
+        
+    def parse_var(self, sheet, row_n):
+        
+        self.sku = sheet['B'+str(row_n)].value
+        self.size = sheet['D'+str(row_n)].value
+        self.price = sheet['E'+str(row_n)].value
+        self.cost = sheet['F'+str(row_n)].value
+        self.weight = sheet['G'+str(row_n)].value
+        self.upc = sheet['H'+str(row_n)].value
+        self.mfg_num = sheet['I'+str(row_n)].value
